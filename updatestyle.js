261deb41679052648bd1549ccfa239101da0c7e8
Quentin Konieczko <quentin-witold.konieczko@ac-lille.fr>
2019-08-11 11:30:23 +0200

    previous_next_links_responsive_progressions

--- updatestyle.js
+++ updatestyle.js
@@ -12,6 +12,8 @@ function updateStyle(){
   styleMenuLinks()
   // set the banner
   setBanner();
+  // set previous next links at the bottom
+  previousNextLinks();
 }
 
 function styleMenuLinks(){
@@ -62,4 +64,111 @@ function setBanner(){
   let imgName = '/banner/back' + nbr + '.jpg'
   document.getElementById("bannerImg").style.backgroundImage = 'url(' + imgName + ')';
   // console.log(imgName);
+}
+
+
+
+function previousNextLinks(){
+  // set the links of the previous and next arrow accordingly
+
+  // first we need to know if there's siblings in the menu to that page
+
+  // how many siblings ?
+  let siblingsNumbers = document.getElementsByClassName("active")[0].parentNode.parentNode.parentNode.parentNode.childElementCount;
+  // console.log("siblingsNumbers");
+  // console.log(siblingsNumbers);
+  // next and previous ?
+  let parent = document.getElementsByClassName("active")[0].parentNode.parentNode.parentNode.parentNode;
+
+  // console.log("parent")
+  // console.log(parent)
+  let siblings = parent.children;
+  // console.log("siblings");
+  // console.log(siblings);
+
+  // we check the number of the current page in the menu.
+  let activeLinkNumber = getActiveLinkNumber(siblings);
+  // console.log(activeLinkNumber)
+
+  // if the number is -1, there was an error while we tried to retrieve it.
+  if (activeLinkNumber != - 1){
+    if (activeLinkNumber > 1){
+      let previousLinkNumber = activeLinkNumber - 1;
+      var previousLinkDest = siblings[previousLinkNumber].firstElementChild.firstElementChild.firstElementChild.href;
+      // console.log(previousLinkDest);
+    }
+    else {
+      let previousLinkDest = null;
+    }
+    if (activeLinkNumber < siblings.length - 1){
+      let nextLinkNumber = activeLinkNumber + 1;
+      var nextLinkDest = siblings[nextLinkNumber].firstElementChild.firstElementChild.firstElementChild.href;
+      // console.log(nextLinkDest);
+    }
+    else {
+      let nextLinkDest = null;
+    }
+    // we set the links to visible and set the hrefs.
+    populateNextAndPreviousLinks([previousLinkDest, nextLinkDest]);
+  }
+}
+
+function getActiveLinkNumber(siblings){
+  // tries to retrieve the number of the active page in the menu.
+
+  try{
+    for (var i = 0; i < siblings.length; i++){
+      sister = siblings[i];
+      // console.log("li sister")
+      // console.log(sister)
+      if (sister.firstElementChild.hasChildNodes() && sister.nodeName == "LI"){
+        // console.log("details")
+        // console.log(sister.firstElementChild);
+
+        let sister_details = sister.firstElementChild;
+        let sister_summary = sister_details.firstElementChild;
+        // console.log("sister_summary");
+        // console.log(sister_summary);
+
+        sister_a = sister_summary.firstElementChild;
+        // console.log("sister_a");
+        // console.log(sister_a);
+        // console.log("sister_a.classList")
+        // console.log(sister_a.classList)
+        // console.log("active" in sister_a.classList)
+        // console.log(i)
+        if (sister_a.classList[0] == "active"){
+          return i;
+        }
+      }
+    }
+  }
+  catch(err){
+    return -1;
+  }
+}
+
+function populateNextAndPreviousLinks(arraylinks){
+  // populate the element of links
+  let previousLinkDest = arraylinks[0];
+  let nextLinkDest = arraylinks[1];
+  // console.log(previousLinkDest);
+  // console.log(nextLinkDest);
+
+  if (previousLinkDest){
+    // console.log("previous !")
+    document.getElementById("link_previous").style.display ='block';
+    document.getElementById("link_previous").href = previousLinkDest;
+  }
+  else {
+    document.getElementById("link_previous").style.display ='none';
+  }
+  if (nextLinkDest){
+    // console.log("next !")
+    document.getElementById("link_next").style.display ='block';
+    document.getElementById("link_next").href = nextLinkDest;
+  }
+  else {
+    document.getElementById("link_next").style.display ='none';
+  }
 }
