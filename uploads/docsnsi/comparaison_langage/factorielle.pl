810d1a32c57c4078a98b8e92e52b5b0a46727977
qkzk <qu3nt1n@gmail.com>
2022-05-28 13:25:15 +0200

    clean everything

--- uploads/docsnsi/comparaison_langage/factorielle.pl
+++
@@ -1,6 +0,0 @@
-factorielle(0,1) :- !.
-factorielle(1,1) :- !.
-factorielle(N, Result) :- N > 0, 
-    					  N_moins_1 is N-1,
-    					  factorielle(N_moins_1,   Fact_N_moins_1),
-    					  Result is N * Fact_N_moins_1.
--- uploads/docsnsi/thonny/demo_00.py
+++
@@ -1,8 +0,0 @@
-def syracuse(u):
-    if u % 2 == 0:
-        res = u // 2
-    else:
-        res = 3 * u + 1
-    return res
-    
-    
