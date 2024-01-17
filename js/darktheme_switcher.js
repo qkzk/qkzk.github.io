56cd5445ffb5aa8ee168a35eadba3fc5dd4bd389
qkzk <qu3nt1n@gmail.com>
2024-01-17 13:10:08 +0100

    remove font awesome

--- js/darktheme_switcher.js
+++ js/darktheme_switcher.js
@@ -3,28 +3,29 @@ var toggle = document.getElementById("dark-mode-toggle");
 var darkTheme = document.getElementById("dark-mode-theme");
 
 toggle.addEventListener("click", () => {
-    if (toggle.className === "toggle_dark") {
-        setTheme("dark");
-    } else if (toggle.className === "toggle_light") {
-        setTheme("light");
-    }
+  if (toggle.className === "toggle_dark") {
+    setTheme("dark");
+  } else if (toggle.className === "toggle_light") {
+    setTheme("light");
+  }
 });
 // the default theme is light
 var savedTheme = localStorage.getItem("dark-mode-storage") || "light";
 setTheme(savedTheme);
 
 function setTheme(mode) {
-    localStorage.setItem("dark-mode-storage", mode);
+  localStorage.setItem("dark-mode-storage", mode);
 
-    if (mode === "dark") {
-        darkTheme.disabled = false;
-        toggle.className = "toggle_light";
-        toggle.innerHTML = '<i class="fas fa-sun"></i>';
-    } else if (mode === "light") {
-        darkTheme.disabled = true;
-        toggle.className = "toggle_dark";
-        toggle.innerHTML = '<i class="fas fa-moon"></i>';
-    }
-    console.log(`Shitched to theme ${mode}`);
-
+  if (mode === "dark") {
+    darkTheme.disabled = false;
+    toggle.className = "toggle_light";
+    // toggle.innerHTML = '<i class="fas fa-sun"></i>';
+    toggle.innerHTML = '<img src="/sun.svg"/>';
+  } else if (mode === "light") {
+    darkTheme.disabled = true;
+    toggle.className = "toggle_dark";
+    // toggle.innerHTML = '<i class="fas fa-moon"></i>';
+    toggle.innerHTML = '<img src="/night.svg"/>';
+  }
+  console.log(`Shitched to theme ${mode}`);
 }
